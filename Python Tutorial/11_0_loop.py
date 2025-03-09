7a7312041ebe073b9bdc6a6275d8c265e56721a9
krohan010 <choudharyrohan010@gmail.com>
2025-03-09 22:27:48 +0530

    loop
    loop
    Continue & Break

---
+++ Python Tutorial/11_0_loop.py
@@ -0,0 +1,20 @@
+# this file contains all loops which exists in python :
+
+fruitList = ["Apple", "Banana", "Mango", "Graphs", "Strawberry"]
+
+# for loop :
+for item in fruitList:
+    print(item)
+
+# using range() function :
+for i in range(5) :
+    print(f"{i} : {fruitList[i]}")
+
+
+# while loop :
+i = 0                           # initialization...
+while i < 5:                    # condition...
+    print(fruitList[i])
+    i = i + 1                   # incre / decre
+
+
---
+++ Python Tutorial/11_1_break_continue.py
@@ -0,0 +1,14 @@
+# Break statement : used to stop iteration in loop
+
+for i in range(10):
+    print(i)
+    if i ==4:
+        break
+
+
+# Continue statement : used to pass an iteration in loop
+print("Continue Statement : ")
+for i in range(10):
+    print(i)
+    if i ==4:
+        continue
